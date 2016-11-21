53444420284a54608fa674f1830bfde58c549838
gabrielnespoli <gabmesnespoli@gmail.com>
2016-11-21 10:18:23 +0100

    passing the project to GIT

---
+++ fourSquareGraph.py
@@ -0,0 +1,39 @@
+import networkx as nx
+import foursquare
+import math
+
+#distance between two points: sqrt((y1 - y0)^2 + (x1 - x0)^2)
+def venues_distance(v1,v2):
+    y1 = v1["pos"][0]
+    x1 = v1["pos"][1]
+    y2 = v2["pos"][0]
+    x2 = v2["pos"][1]
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+
+def generate_graph():
+    CLIENT_ID = "1QCKNMZX3B21SMZGOF5ZQALB33BQP20TKARFQLDBTOKDESGS"
+    CLIENT_SECRET = "2PQCTZUW2PDEIL50JOF0BNHKJXJC2MLSB0FI5LSABPKXQKES"
+    client = foursquare.Foursquare(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+    G = nx.Graph()
+    search = client.venues.explore(params={'near':'Rome,Italy'})
+    for group in search['groups']:
+        #creates all the nodes
+        for venue in group['items']:
+            G.add_node(venue["venue"]["id"],
+                       id=venue["venue"]["id"],
+                       name=venue["venue"]["name"],
+                       pos=(venue["venue"]["location"]["lat"],venue["venue"]["location"]["lng"]),
+                       country=venue["venue"]["location"]["country"],
+                       size=10)
+
+        #creates the edges between next venues
+        for venue in group['items']:
+            nextVenues = client.venues.nextvenues(venue["venue"]["id"])
+            for nextvenue in nextVenues["nextVenues"]["items"]:
+                v1 = G.node[venue["venue"]["id"]]
+                if(nextvenue["id"] in G):
+                    v2 = G.node[nextvenue["id"]]
+                    G.add_edge(v1["id"], v2["id"], distance=venues_distance(v1, v2))
+    return G
---
+++ testingfoursquare.py
@@ -0,0 +1,57 @@
+import foursquare
+import fourSquareGraph as lib
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def main():
+    CLIENT_ID = "1QCKNMZX3B21SMZGOF5ZQALB33BQP20TKARFQLDBTOKDESGS"
+    CLIENT_SECRET = "2PQCTZUW2PDEIL50JOF0BNHKJXJC2MLSB0FI5LSABPKXQKES"
+    client = foursquare.Foursquare(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    #
+    # res = client.venues("4c31a1e26f1fef3b440dec3d")
+    # print(res["venue"]["name"])
+    # print(res["venue"]["hours"])
+    # print(res["venue"]["popular"])
+    # print(res["venue"]["location"]["lat"])
+    # print(res["venue"]["location"]["lng"])
+    #
+    # #'ll search by latitude and longitude
+    # search = client.venues.explore(params={'ll':'41.89,12.50'})
+    # for group in search['groups']:
+    #     print(group["name"])
+    #     for nv in group['items']:
+    #         print(nv["venue"]["name"])
+    # print("---------END---------")
+    #
+    # search = client.venues.explore(params={'near':'Rome,Italy'})
+    # for group in search['groups']:
+    #     print(group["name"])
+    #     for nv in group['items']:
+    #         print(nv["venue"]["name"])
+    # print("---------END---------")
+    #
+    # search = client.venues.explore(params={'near':'Rome,Italy','query':'transtevere'})
+    # for group in search['groups']:
+    #     print(group["name"])
+    #     for nv in group['items']:
+    #         print(nv["venue"]["name"])
+    # print("---------END---------")
+    #
+    # next_venues = client.venues.nextvenues("4c31a1e26f1fef3b440dec3d")
+    # for nv in next_venues['nextVenues']['items']:
+    #     print(nv["name"])
+    # print("---------END---------")
+
+    G = lib.generate_graph()
+    for edge in G.edges():
+        print(client.venues(edge[0])["venue"]["name"]," <- is near -> ", client.venues(edge[1])["venue"]["name"])
+
+    plt.clf()
+    nx.draw_networkx_nodes(G, nx.get_node_attributes(G, 'pos'),
+                           node_size=[s for s in nx.get_node_attributes(G,"size").values()])
+    nx.draw_networkx_edges(G, nx.get_node_attributes(G, 'pos'), width=0.2, alpha=0.5)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
